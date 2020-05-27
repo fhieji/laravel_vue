0ec9e286dc2ad079986a4d0394e59fbf167e0a4c
vagrant <vagrant@homestead>
2020-05-27 15:22:58 +0000

    Extend form functionality to be able to update question

--- app/Question.php
+++ app/Question.php
@@ -5,19 +5,18 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-
 class Question extends Model
 {
     protected $fillable = ['title', 'body'];
-
-    public function user(){
+    
+    public function user() {
         return $this->belongsTo(User::class);
-    }
+    }    
 
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
-        $this->attributes['slug'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
     }
 
     public function getUrlAttribute()
@@ -27,8 +26,17 @@ class Question extends Model
 
     public function getCreatedDateAttribute()
     {
-        
         return $this->created_at->diffForHumans();
     }
-    
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
