1a14e9a6b7717d1b384cd474bdca69cba91f7b70
vagrant <vagrant@homestead>
2020-06-08 16:14:14 +0000

    Implenent many to many polymorhpic relationship and make the vote question control working

--- app/User.php
+++ app/User.php
@@ -56,4 +56,32 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Question::class, 'favorites'); //, 'author_id', 'question_id');
     }
+
+    public function voteQuestions()
+    {
+        return $this->morphedByMany(Question::class, 'votable');
+    }
+
+    public function voteAnswers()
+    {
+        return $this->morphedByMany(Answer::class, 'votable');
+    }
+
+    public function voteQuestion(Question $question, $vote) 
+    {
+        $voteQuestions = $this->voteQuestions();
+        if($voteQuestions->where('votable_id', $question->id)->exists()){
+            $voteQuestions->updateExistingPivot($question,['vote'=>$vote]);
+        }else{
+            $voteQuestions->attach($question, ['vote'=> $vote]);
+        };
+
+        $question->load('votes');
+        $downVotes = (int) $question->downVotes()->wherePivot('vote',-1)->sum('vote');
+        $upVotes = (int) $question->upVotes()->wherePivot('vote',1)->sum('vote');
+        $question->votes_count = $upVotes + $downVotes;
+        $question->save();
+    }
+
+
 }
