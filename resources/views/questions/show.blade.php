a1ae5a86bf27ebc2e4adb3da8cef245c0ae3fa24
vagrant <vagrant@homestead>
2020-06-17 15:49:15 +0000

    error in showing user-Info

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -8,41 +8,39 @@
                 <div class="card-body">
                     <div class="card-title">
                         <div class="d-flex align-items-center">
-                            <h1>{{ $question-> title }}</h1>
+                            <h1>{{ $question->title }}</h1>
                             <div class="ml-auto">
                                 <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
                             </div>
-                        </div>
+                        </div>                        
                     </div>
 
                     <hr>
 
                     <div class="media">
-                        @include('shared._vote', [
+                        @include ('shared._vote', [
                             'model' => $question
                         ])
+                        
                         <div class="media-body">
-                            {{  $question->body  }}
-                            <div class="float-right">
-                                <div class="row">
-                                    <div class="col-4"></div>
-                                    <div class="col-4"></div>
-                                    <div class="col-4">
-                                        <user-info :model="{{ $question }}" label="Asked"></user-info>
-                                    </div>
+                            {!! $question->body_html !!}
+                            <div class="row">
+                                <div class="col-4"></div>
+                                <div class="col-4"></div>
+                                <div class="col-4">                                    
+                                    <user-info :model="{{ $question }}" label="Asked"></user-info>
                                 </div>
-                        </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @include('answers._index', [
+    @include ('answers._index', [
         'answers' => $question->answers,
         'answersCount' => $question->answers_count,
     ])
-
-    @include('answers._create')
+    @include ('answers._create')
 </div>
 @endsection
