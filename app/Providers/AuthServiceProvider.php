fe69c14961e532a7c74e9ade01b26bca337dfc86
vagrant <vagrant@homestead>
2020-08-15 07:11:01 +0000

    Install & onfigure Laravel for API Authentification

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Gate;
 use App\Question;
 use App\Answer;
 use App\Policies\AnswerPolicy;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -37,5 +38,7 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('delete-question', function($user, $question){
             return $user->id === $question->user_id;
         });
+
+        Passport::routes();
     }
 }
